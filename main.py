d4165917e3c55a11ea632192905591f11fd9c116
Apoll1on <aseeliger@me.com>
2022-05-07 19:23:50 +0200

    Updated

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ if __name__ == '__main__':
 
 
     def g(i, a):
-        return np.ei * (-a * i * h ** 2)
+        return np.exp(-a * i * h ** 2)
 
     def DUDX(u):
         diff = np.zeros_like(u)
