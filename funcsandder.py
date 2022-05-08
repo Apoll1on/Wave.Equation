752fd6ac1fe54926f9ed43be7d55315d9388156b
Apoll1on <aseeliger@me.com>
2022-05-08 12:40:01 +0200

    Updated

---
+++ funcsandder.py
@@ -0,0 +1,66 @@
+import numpy as np
+
+a = 0
+b = 1
+n = 20
+h = 1 / (n - 1)
+
+
+def DUDX(u):
+    diff = np.zeros_like(u)
+    diff[1:-1] = (u[2:] - u[:-2]) / (2 * h)
+    return diff
+
+
+def D2UDX2(u):
+    diff = np.zeros_like(u)
+    diff[1:-1] = (u[2:] - 2 * u[1:-1] + u[:-2]) / (h * h)
+    return diff
+
+
+def f1(x):
+    return (x - 1 / 2) ** 2 + x
+
+
+def f2(x):
+    return (x - 1 / 2) ** 3 + (x - 1 / 2) ** 2 + x
+
+
+def f3(x):
+    return np.sqrt(x)
+
+
+def s1(x):
+    return np.sin(12 * np.pi * x)
+
+
+def s2(x):
+    return np.sin(12 * np.pi * x) ** 4
+
+
+def g(x, a):
+    return np.exp(-a * x ** 2)
+
+
+def Df1DX(x):
+    return 2 * x
+
+
+def Df2DX(x):
+    return 3 * (x) ** 2 - x + 3 / 4
+
+
+def Df3DX(x):
+    return 0.5 / np.sqrt(x)
+
+
+def Ds1DX(x):
+    return 12 * np.pi * np.cos(12 * np.pi * x)
+
+
+def Ds2DX(x):
+    return 48 * np.pi * np.sin(12 * np.pi * x) ** 3 * np.cos(12 * np.pi * x)
+
+
+def DgDX(x, a):
+    return -2 * a * x * np.exp(-a * (x) ** 2)
---
+++ rungekutta.py
@@ -0,0 +1,14 @@
+def RK4(f, u0, delt, iter, fileName):
+    f = open("calculateddata.txt", "a")
+    t = 0
+    u = u0
+
+    for _ in range(iter):
+        k1 = f(t, u)
+        k2 = f(t + delt / 2, u0 + delt * k1 / 2)
+        k3 = f(t + delt / 2, u0 + delt * k2 / 2)
+        k4 = f(t + delt, u0 + k3)
+        u = u + delt * (k1 / 6 + k2 / 3 + k3 / 3 + k4 / 6)
+
+    f.close()
+    return u
