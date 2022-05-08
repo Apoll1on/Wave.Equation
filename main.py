c52ab26e79919455d6ad7ef3b2a794364fb92283
Apoll1on <aseeliger@me.com>
2022-05-08 14:08:54 +0200

    Updated

--- main.py
+++ main.py
@@ -14,11 +14,73 @@ def plotting(xvalues, mathfunc, derivative, numder, funcname):
     plt.show()
 
 
+# For Harmonic Oscillator we get:
+# dx/dt=p firsteq
+# dp/dt=-x by Newton Law
+# Therefore runge kutta for both:
+def firsteq(t, p):
+    return p
+
+
+def secondeq(t, x):
+    return -x
+
+
 if __name__ == '__main__':
 
-# Aufgabe 6
-# Hamiltonian Function:
-    
+    # Aufgabe 6
+    fileName = "calculateddata.txt"
+
+    x0 = 1
+    p0 = 0
+    tsteps = 100
+    delt = 2 * np.pi / tsteps
+    tmax = 2 * np.pi
+    t0 = 0
+
+    f = open(fileName, "a")
+    f.write("\n\nNew Run")
+
+    x = x0
+    p = p0
+    t = t0
+    iter = 0
+    tarray = np.zeros(tsteps + 1)
+    xarray = np.zeros(tsteps + 1)
+    parray = np.zeros(tsteps + 1)
+
+    while t < tmax:
+        k1x = firsteq(t, p)
+        k1p = secondeq(t, x)
+        k2x = firsteq(t + delt / 2, p + delt * k1p / 2)
+        k2p = secondeq(t + delt / 2, x + delt * k1x / 2)
+        k3x = firsteq(t + delt / 2, p + delt * k2p / 2)
+        k3p = secondeq(t + delt / 2, x + delt * k2x / 2)
+        k4x = firsteq(t + delt, p + k3p)
+        k4p = secondeq(t + delt, x + k3x)
+
+        x = x + delt * (k1x / 6 + k2x / 3 + k3x / 3 + k4x / 6)
+        p = p + delt * (k1p / 6 + k2p / 3 + k3p / 3 + k4p / 6)
+
+        xarray[iter] = x
+        parray[iter] = p
+        tarray[iter] = t
+
+        f.write("\n")
+        f.write("Time: " + str(t) + "  p: " + str(p) + "  x: " + str(x))
+
+        t += delt
+        iter += 1
+
+    f.close()
+
+    fig, ax = plt.subplots(2)
+    ax[0].plot(tarray, xarray)
+    ax[1].plot(tarray, parray)
+
+    fig1, ax1 = plt.subplots()
+    ax1.plot(xarray, parray)
+    plt.show()
 
 # Aufgabe 5
 # taking n,a,b,h from funcsandder
--- rungekutta.py
+++ rungekutta.py
@@ -1,18 +1,22 @@
 from typing import Callable
 
 
-def RK4(f: Callable, u0, delt, iter, fileName: str):
+def RK4(f: Callable, u0, delt, t0, tmax, fileName: str):
     f = open(fileName, "a")
     f.write("New Run")
-    t = 0
-    u = u0
+    iter = (tmax - t0) / delt
 
-    for _ in range(iter):
+    u = u0
+    t = t0
+
+    for it in range(iter):
         k1 = f(t, u)
         k2 = f(t + delt / 2, u0 + delt * k1 / 2)
         k3 = f(t + delt / 2, u0 + delt * k2 / 2)
         k4 = f(t + delt, u0 + k3)
         u = u + delt * (k1 / 6 + k2 / 3 + k3 / 3 + k4 / 6)
+        t += delt
+
+        f.write("Iteration: " + it + "   Time: " + t + "  u: " + u)
 
     f.close()
-    return u
