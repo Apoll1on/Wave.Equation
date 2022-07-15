5287edf3a1b8809106d2ec24974c9e8b2a56cf6b
Linrael <jpgrandm@gmail.com>
2022-07-15 14:20:24 +0200

    ?!?

--- a8.py
+++ a8.py
@@ -6,11 +6,11 @@ import numpy as np
 
 
 def stabtest():
-    a = 101
+    a = 201
     c = 100
     linestoread = [c] # int(0 * c), int(c * 0.125), int(c * 0.25), int(c * 0.375), int(c * 0.5), int(c * 0.625), int(c * 0.75), int(c * 0.875),
     xarray, times, phiarray, piarray = solving(a, c + 1, linestoread=linestoread, alpha=0.1)
-    #xarray2, times2, phiarray2, piarray2 = solving(2 * a - 1, c + 1, linestoread=linestoread, alpha=0.2)
+    xarray2, times2, phiarray2, piarray2 = solving(2 * a - 1, c + 1, linestoread=linestoread, alpha=0.2)
     #xarray4, times4, phiarray4, piarray4 = solving(4 * (a - 1) + 1, c + 1, linestoread=linestoread, alpha=0.4)
 
     # fig1, ax1 = plt.subplots(3, 3)
@@ -53,8 +53,8 @@ def stabtest():
     # print(ma)
 
     fig3, ax3 = plt.subplots(1)
-    ax3.plot(xarray, phiarray[0]) # - phiarray2[0][::2]
-    #ax3.plot(xarray, phiarray2[0][::2] + 1) # - phiarray4[0][::4]
+    ax3.plot(xarray, phiarray[0] ) #- phiarray2[0][::2]
+    #ax3.plot(xarray, phiarray2[0][::2] - phiarray4[0][::4]) #
     #ax3.plot(xarray, phiarray4[0][::4])
 
 
