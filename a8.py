01970a8b105352d840c2e02a8741365ff48e6dfd
Linrael <jpgrandm@gmail.com>
2022-07-20 10:58:03 +0200

    ?!?

--- a8.py
+++ a8.py
@@ -50,17 +50,17 @@ def convergence():
 
 
 def selfconvergence():
-    a = 101
+    a = 201
     c = 1000
-    linestoread = [
-        c - 1]  # 0, int(c * 0.125), int(c * 0.25), int(c * 0.375), int(c * 0.5), int(c * 0.625), int(c * 0.75), int(c * 0.875),
+    linestoread = [c - 1]  # 0, int(c * 0.125), int(c * 0.25), int(c * 0.375), int(c * 0.5), int(c * 0.625), int(c * 0.75), int(c * 0.875),
     xarray, times, phiarray, piarray = solver.solving(a, c + 2, linestoread=linestoread, alpha=.1)
-    xarray2, times, phiarray2, piarray2 = solver.solving(2 * a - 1, c + 2, linestoread=linestoread)
-    xarray4, times, phiarray4, piarray4 = solver.solving(4 * (a - 1) + 1, c + 2, linestoread=linestoread)
+    xarray2, times, phiarray2, piarray2 = solver.solving(2 * a - 1, c + 2, linestoread=linestoread, alpha=.2)
+    xarray4, times, phiarray4, piarray4 = solver.solving(4 * (a - 1) + 1, c + 2, linestoread=linestoread, alpha=.4)
 
     fig, ax = plt.subplots(1)
     ax.plot(xarray, (phiarray[0] - phiarray2[0][::2]), label='h - h/2')  #
     ax.plot(xarray, 16 * (phiarray2[0][::2] - phiarray4[0][::4]), label='h/2 - h/4')  #
     # ax3.plot(xarray, phiarray4[0][::4])
 
+    ax.legend()
     plt.show()
