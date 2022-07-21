78044ddb0fd404bba32919445326af3b99807546
Linrael <jpgrandm@gmail.com>
2022-07-21 17:49:22 +0200

    ?!?

--- main.py
+++ main.py
@@ -1,48 +1,42 @@
-from matplotlib import pyplot as plt
+import funcsandder
+import numpy as np
+
+#xsteps
+x0=0
+xmax=1
+xpoints=101
 
 
-# hh
+#timesteps
+periods=10
+timesteps=periods*1000#number of timesteps
+t0=0 #starting time
 
 
-def plotting(xvalues, mathfunc, derivative, numder, funcname):
-    values = mathfunc(xvalues)
-    fig, ax = plt.subplots(3)
-    ax[0].plot(xvalues, derivative(xvalues))
-    ax[1].plot(xvalues, numder(values))
-    ax[2].plot(xvalues, numder(values) - derivative(xvalues))
-    plt.title(funcname)
-    plt.show()
+#alpha
+alpha=1
+
+#Initial conditions
+xarray = np.array(np.linspace(x0, xmax, xpoints), dtype=np.double)
+# phiinit=funcsandder.gausswave(xarray,-20.,0.05)#phi
+# piinit=funcsandder.dergaus(xarray,-20,0.05)#pi
+phiinit = funcsandder.s1(xarray)
+piinit=np.zeros(xpoints,dtype=np.double)
+
+#Boundary condition
+boundaryCondition="extrapolation"
+
+#read data
+fileName="calculateddata.txt"
+linestoread = [0]
+for i in range(1, periods):
+    linestoread.append(i * 1000)
 
 
-# For Harmonic Oscillator we get:
-# dx/dt=p firsteq
-# dp/dt=-x by Newton Law
-# Therefore runge kutta for both:
 
 
+a8new.stabtest(x0,xmax,xpoints,t0,timesteps,alpha,phiinit,piinit,boundaryCondition,fileName,linestoread)
 
-if __name__ == '__main__':
-    # Aufgabe 6
-    # rk.a6()
-    # Aufgabe
-    # a5.plot()
-    # taking n,a,b,h from funcsandder
-    # ugly but works...
-
-    # Omega = np.linspace(a, b, n)
-    # i_count = np.linspace(0, n - 1, n)
-
-    # plotting(Omega, f1, Df1DX, DUDX, "f1")
-    # plotting(Omega, f2, Df2DX, DUDX, "f2")
-    # print(Omega)
-    # print(f1(Omega))
-    # print(DUDX(f1(Omega)))
-    # print(Df1DX(Omega))
-    #
-    # aufgabe 7
-    # a7.solving(1001, 1000, "calculateddata", "periodic")
-
-    # a8
-    a8.stabtest()
-   # testa10.plotten()
-
+#testa10new.calcplot(x0,xmax,xpoints,t0,timesteps,alpha,phiinit,piinit,boundaryCondition,fileName,linestoread)
+#testa10new.plotten(xpoints,xarray,linestoread,fileName)
+#a8.stabtest()
