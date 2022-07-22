6060f018f897cad564b7a8877ba6841173e93f62
Linrael <jpgrandm@gmail.com>
2022-07-22 16:21:22 +0200

    new

--- a8.py
+++ a8.py
@@ -1,5 +1,4 @@
 import numpy as np
-
 import funcsandder
 import solver
 from matplotlib import pyplot as plt
--- for_Rossella.py
+++ for_Rossella.py
@@ -1,7 +1,11 @@
 import numpy as np
 import misc
 import os
+import funcsandder
+from matplotlib import pyplot as plt
 
+
+#### solver file:
 
 def boundaryConditions(u, boundaryCondition, delx, alpha=1, phi_old_left=0, phi_old_right=0):
     if boundaryCondition == "periodic":
@@ -121,3 +125,113 @@ def solving(x0, xmax, xpoints, t0, timesteps, alpha,
     f.close()
     times, phiarray, piarray = misc.readdata(fileName, xpoints, lines=linestoread)
     return xarray, times, phiarray, piarray
+
+
+#### a8 file:
+
+def stabtest(x0, xmax, xpoints, t0, timesteps, alpha, phiinit, piinit, boundaryCondition, fileName, linestoread):
+    xarray, times, phiarray, piarray = solving(x0, xmax, xpoints, t0, timesteps + 2, alpha,
+                                                      phiinit, piinit, boundaryCondition, fileName, linestoread)
+    print(times)
+
+    fig, ax = plt.subplots(2, 1)
+    c = plt.get_cmap('gist_rainbow')
+    n = len(linestoread)
+    for i in range(n):
+        # print("Phi " + str(i) + " :")
+        # print(np.mean(phiarray[i]))
+        # print("Pi " + str(i) + " :")
+        # print(np.mean(piarray[i]))
+        ax[0].plot(xarray, phiarray[i, :], label=format(float(times[i]), '.4f'), color=c(i / (n - 1)))
+        ax[1].plot(xarray, piarray[i, :], label=format(float(times[i]), '.4f'), color=c(i / (n - 1)))
+
+    ax[0].set_title('phi')
+    ax[1].set_title('pi')
+    ax[0].legend()
+    ax[1].legend()
+    plt.show()
+
+
+# plot q over time for every timestep
+def convergence(x0, xmax, xpoints, t0, timesteps, alpha,
+                phiinit, piinit, boundaryCondition, fileName, linestoread, periods):
+    timesteps = periods * (xpoints - 1) / alpha
+    linestoread = [0]
+    for i in range(1, periods + 1):
+        linestoread.append(int(i * (xpoints - 1)))
+    xarray, times, phiarray, piarray = solving(x0, xmax, xpoints, t0, timesteps + 2, alpha,
+                                                      phiinit, piinit, boundaryCondition, fileName, linestoread)
+    print("alpha", alpha, "\ntimes", times)
+    fig, ax = plt.subplots(1)
+    for i in range(len(linestoread)):
+        ax.plot(xarray, phiarray[i, :], label=format(float(times[i]), '.4f'))
+
+    ax.legend()
+    ax.set_title("alpha" + format(float(alpha), '.2f'))
+    plt.show()
+
+
+def selfconvergence(x0, xmax, xpoints, t0, timesteps, alpha,
+                    phiinit, piinit, boundaryCondition, fileName, linestoread):
+    func_phiint = funcsandder.s1
+    func_piint = np.zeros
+
+    linestoread = [timesteps - 1]
+    xarray, times, phiarray, piarray = solving(x0, xmax, xpoints, t0, timesteps + 2, 0.1,
+                                                      func_phiint(np.linspace(x0, xmax, xpoints)), func_piint(xpoints), boundaryCondition, fileName,
+                                                      linestoread)
+    xarray2, times, phiarray2, piarray2 = solving(x0, xmax, 2 * xpoints - 1, t0, timesteps + 2, 0.2,
+                                                         func_phiint(np.linspace(x0, xmax, 2 * xpoints - 1)), func_piint(2 * xpoints - 1),
+                                                         boundaryCondition, fileName, linestoread)
+    xarray4, times, phiarray4, piarray4 = solving(x0, xmax, 4 * (xpoints - 1) + 1, t0, timesteps + 2, 0.4,
+                                                         func_phiint(np.linspace(x0, xmax, 4 * (xpoints - 1) + 1)), func_piint(4 * (xpoints - 1) + 1),
+                                                         boundaryCondition, fileName, linestoread)
+
+    fig, ax = plt.subplots(1)
+    ax.plot(xarray, (phiarray[0] - phiarray2[0][::2]), label='h - h/2')  #
+    ax.plot(xarray, 4 * (phiarray2[0][::2] - phiarray4[0][::4]), label='h/2 - h/4')  #
+    # ax.plot(xarray, phiarray[0], label = 'h')
+    # ax.plot(xarray, phiarray2[0][::2], label = 'h/2')
+    # ax.plot(xarray, phiarray4[0][::4], label = 'h/4')
+
+    ax.legend()
+    plt.show()
+
+
+#### main file:
+
+
+# xsteps
+x0 = 0
+xmax = 1
+xpoints = 2001
+
+# timesteps
+timesteps = 2000  # number of timesteps, factor has to be the same as in " #read data"
+tcount = 25  # Number of lines to read out/plot
+t0 = 0  # starting time
+
+# alpha
+alpha = 1
+
+# Initial conditions
+xarray = np.array(np.linspace(x0, xmax, xpoints), dtype=np.double)
+phiinit = funcsandder.gausswave(xarray, 0.5, 0.05)
+piinit = funcsandder.dergaus(xarray, 0.5, 0.05)
+# phiinit = funcsandder.s1(xarray)
+# piinit = np.zeros(xpoints)
+
+
+# phiinit = np.zeros(xpoints)
+# phiinit[int(-x0 * (xpoints - 1) / (xmax - x0)): int((1-x0) * (xpoints - 1) / (xmax - x0) + 1)] = funcsandder.s1(np.linspace(0, 1, int((xpoints - 1) / (xmax - x0) + 1)))
+
+# Boundary condition
+boundaryCondition = "FDstencil"
+
+# read data
+fileName = "calculateddata.txt"
+linestoread = [0]
+for i in range(1, tcount + 1):
+    linestoread.append(i * timesteps / tcount)
+
+stabtest(x0, xmax, xpoints, t0, timesteps, alpha, phiinit, piinit, boundaryCondition, fileName, linestoread)
